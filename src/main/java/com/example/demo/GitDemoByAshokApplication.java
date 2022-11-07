4292e03d8a26d445938376dcfc6ce36b4ee5e3d1
ryearva <rramanareddy28@gmail.com>
2022-11-07 16:23:10 +0530

    first initial commit

---
+++ src/main/java/com/example/demo/GitDemoByAshokApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoByAshokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoByAshokApplication.class, args);
+	}
+
+}
